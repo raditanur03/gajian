2c044c76b3037a32877e2e1d8f23a70e1c182779
raditanur03 <raditanh2003@gmail.com>
2024-06-05 09:47:16 +0700

    Add files via upload

---
+++ tambah_gaji.php
@@ -0,0 +1,20 @@
+<?php
+try{
+    $host = "localhost";
+    $dbname = "gajian";
+    $username = "root";
+    $pass = "";
+    $koneksi = new PDO("mysql:host=" . $host . ";dbname=". 
+    $dbname, $username, $pass);
+    $koneksi->exec("set names utf8");
+
+    $sql = "INSERT into tbl_gaji values(null,'badi','pegawai','1000000','6789012')";
+    $result = $koneksi->query($sql)->fetch();
+
+    var_dump($result);
+
+}catch(PDOException $exception){
+    echo "Database could not be connected: " . $exception->getMessage();
+}
+
+?>
